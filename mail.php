e412f36e9a368957d32df8bc5a7b1157321c5fdb
hron2017 <andreypbiz@gmail.com>
2020-06-06 11:55:02 +0300

    comit

--- mail.php
+++ mail.php
@@ -1,36 +1,24 @@
 <?php
 	$method = $_SERVER['REQUEST_METHOD'];
-     
-  
+      
   $mail_to = 'andreypbiz@gmail.com';     
 
- 
- if(!$_POST['userName']){
-  $userEmail  = $_POST['userEmail'];
-  
-  $message .= "Адрес пользователя: " . $userEmail . "<br/>";
-  $subject = "Получение Урока";   
-  
- } else {
+ if($_POST){
+
   $userName   = $_POST['userName'];
-  $userEmail  = $_POST['userEmail'];
   $userPhone  = $_POST['userPhone'];
-  $userComent  = $_POST['userComent'];
- 
   $message  = "Имя пользователя: " . $userName . "<br/>";  
   $message .= "Телефон пользователя: " . $userPhone . "<br/>";
-  $message .= "Email пользователя: " . $userEmail . "<br/>";
-  $message .= "Комментарий пользователя: " . $userComent . "<br/>";
 
   $subject = "Получить Курс";   
  }
  
 
   $headers= "MIME-Version: 1.0\r\n";
-  $headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
-  $headers .= "From:" . $userPhone . "<no-reply@test.com>\r\n"; // от кого письмо
+  $headers .= "Content-type: text/html; charset=utf-8\r\n"; 
+  $headers .= "From:" . $userPhone . "<no-reply@test.com>\r\n"; 
   
-  // отправляем письмо 
+ 
   $mail = mail($mail_to, $subject, $message, $headers);
   
   if($mail){
